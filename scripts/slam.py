0c2c33f0b3cb019d04ae23f9d2badd79045801a8
plehtela <paavo.lehtela@aalto.fi>
2022-03-22 15:01:19 +0200

    wrote more code on slam.py

--- scripts/slam.py
+++ scripts/slam.py
@@ -10,33 +10,54 @@ import pcl.pcl_visualization
 
 VERBOSE = False
 
-def callback(data):
-  if VERBOSE:
-    print('received image of type: "%s"' % data.format)
+class localizer:
 
-  points_list = []
+  def __init__():
   
-  for data in pc2.read_points(data, skip_nans=True):
-    points_list.append([data[0], data[1], data[2]])
-
-  pcl_data = pcl.PointCloud()
-  pcl_data.from_list(points_list)
-
-  visual = pcl.pcl_visualization.CloudViewing()
-
-  visual.ShowMonochromeCloud(pcl_data, b'cloud')
-
-  v = True
-  while v:
-    v=not(visual.WasStopped())
+   
+class mapper:
   
+  def __init__():
+    self.M = pcl.PointCloud()
+    self.T = 0
 
+  def get_map():
+    return self.M
 
+  def update_map(data):
+    if VERBOSE:
+      print('received poincloud data from the depth camera')
+
+    points_list = []
+  
+    for data in pc2.read_points(data, skip_nans=True):
+      points_list.append([data[0], data[1], data[2]])
+
+    pcl_data = pcl.PointCloud()
+    pcl_data.from_list(points_list)
+  
+    #if Map does not
+    if self.M.empty():
+      self.M = pcl_data
+    else:
+      
+      
+  def get_location():
+    return self.T
+
+  #Update the current location self.T using nav_msgs/Odometry.msg message
+  def update_location(data):
+ 
+#listener listens to 'camera/depth/points' and runs callback function when data is received
 def listener():
 
   rospy.init_node('pointcloud_listener', anonymous=True)
+  rospy.init_node('IMU_listener', anonymous=True)
   
-  rospy.Subscriber('camera/depth/points', PointCloud2, callback)
+  Map = mapper()
+
+  rospy.Subscriber('camera/depth/points', PointCloud2, Map.update_map)
+  rospy.Subscriber('IMU', , Map.update_location)
 
   rospy.spin()
 
