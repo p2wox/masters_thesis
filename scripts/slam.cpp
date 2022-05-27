019c47bdf132cbf9fb62cee49b7bb708c8b0906d
plehtela <paavo.lehtela@aalto.fi>
2022-05-27 13:12:11 +0300

    Fixed errors

--- scripts/slam.cpp
+++ scripts/slam.cpp
@@ -3,26 +3,36 @@
 #include <pcl/PCLPointCloud2.h>
 //#include <pcl/conversions.h>
 #include <pcl_ros/transforms.h> 
+#include <pcl/common/eigen.h>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <Eigen/Geometry>
 #include <ros/callback_queue.h>
 #include "nav_msgs/Odometry.h"
 #include "sensor_msgs/PointCloud2.h"
+#include <iostream>
 
 int VERBOSE = 0;
 
    
 class mapper{
   public:
+    mapper(){
+      T = Eigen::Transform<tfScalar, 3, Eigen::Affine>::Identity();
+      //M = new pcl::PointCloud<pcl::PointXYZ>;
+    }
     //Return the current map
-    pcl::PointCloud<pcl::PointXYZ>::Ptr get_map(){
+    pcl::PointCloud<pcl::PointXYZ> get_map(){
       return M;
     }
 
     sensor_msgs::PointCloud2 get_map_msg(){
+      pcl::PCLPointCloud2 pcl2_data;
+
+      pcl::toPCLPointCloud2(M, pcl2_data);
+
       sensor_msgs::PointCloud2 map_msg;
-      pcl_conversions::fromPCL(M, map_msg);
+      pcl_conversions::fromPCL(pcl2_data, map_msg);
       return map_msg;
     }
     
@@ -33,43 +43,45 @@ class mapper{
       //Convert data from PointCloud2 to pcl
       pcl::PCLPointCloud2 pcl2_data;
       pcl_conversions::toPCL(*data,pcl2_data);
-      pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_data(new pcl::PointCloud<PCL::PointXYZ>);
-      pcl::fromPCLPointCloud2(pcl2_data, *pcl_data);
+      pcl::PointCloud<pcl::PointXYZ> pcl_data;
+      pcl::fromPCLPointCloud2(pcl2_data, pcl_data);
       
       //if Map does not exist yet, create map from the current data received fromt the depth sensor
       if(M.empty()) {
-        &M = &pcl_data;
-      }else:
+        M = pcl_data;
+      } else{
         //transform point cloud to correct location using current location of the robot
-        pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_data_transformed(new pcl::PointCloud<PCL::PointXYZ>);
+        pcl::PointCloud<pcl::PointXYZ> pcl_data_transformed;
 
-        pcl::transformPointCloud(*pcl_data, *pcl_data_transformed, T);
+        pcl::transformPointCloud(pcl_data, pcl_data_transformed, T);
 
         //Match point cloud to the map
 
       
         //Combine pointclouds
         M += pcl_data_transformed;
+      }
     }      
 
     //Return the current location of the robot    
-    Eigen::Transform<Scalar, 3, Eigen::Affine> get_location(){
+    Eigen::Transform<tfScalar, 3, Eigen::Affine> get_location(){
       return T;
     }
 
-    nav_msgs::Odometry get_location_msg{
+    nav_msgs::Odometry get_location_msg(){
       nav_msgs::Odometry location_msg;
   
-      location_msg->pose.pose.position.x = T.translation()[0];      
-      location_msg->pose.pose.position.y = T.translation()[1];      
-      location_msg->pose.pose.position.z = T.translation()[2];      
+      location_msg.pose.pose.position.x = T.translation()[0];      
+      location_msg.pose.pose.position.y = T.translation()[1];      
+      location_msg.pose.pose.position.z = T.translation()[2];      
 
-      Quaternionf q(T.linear());
-      
-      data-pose.pose.orientation.x = q.x;
-      data-pose.pose.orientation.y = q.y;
-      data-pose.pose.orientation.z = q.z;
-      data-pose.pose.orientation.w = q.w;
+      Eigen::Quaterniond q;
+      q = T.linear();
+  
+      location_msg.pose.pose.orientation.x = q.x();
+      location_msg.pose.pose.orientation.y = q.y();
+      location_msg.pose.pose.orientation.z = q.z();
+      location_msg.pose.pose.orientation.w = q.w();
 
       return location_msg;
     }
@@ -83,6 +95,7 @@ class mapper{
       T.translation()[1] = data->pose.pose.position.y;
       T.translation()[2] = data->pose.pose.position.z;
 
+      std::cout << data->pose.pose.position.x << '\n';
       //add orientation part of the odometry data to transformation matrix
 
       Eigen::Quaterniond r;
@@ -91,7 +104,7 @@ class mapper{
       r.z() = data->pose.pose.orientation.z;
       r.w() = data->pose.pose.orientation.w;
 
-      t.linear() = r.normalized().toRotationMatrix();
+      T.linear() = r.normalized().toRotationMatrix();
       
       /*
       for(int i=0;i<3;i++){
@@ -104,25 +117,26 @@ class mapper{
 
   private:
     //Initialize the map to an empty point cloud
-    pcl::PointCloud<pcl::PointXYZ>::Ptr M(new pcl:PointCloud<PCL::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ> M;
 
     //Initialize location of the robot to zero 
-    Eigen::Transform<Scalar, 3, Eigen::Affine> T = Eigen::Transform<Scalar, 3, Eigen::Affine>::identity();
-}
+    Eigen::Transform<tfScalar, 3, Eigen::Affine> T;
+};
+
 
 int main(int argc, char **argv){
   ros::init(argc, argv, "slam_listerer");
   ros::NodeHandle n;
 
-  pcl::PointCloud Map = mapper();
+  mapper Map;
 
-  ros::Subscriber pc_sub = n.subscribe("camera/depth/points", 1000, Map.update_map);
+  ros::Subscriber pc_sub = n.subscribe("camera/depth/points", 1000, &mapper::update_map, &Map);
 
-  ros::Subscriber IMU_sub = n.subscribe("IMU", 1000, Map.update_location);
+  ros::Subscriber IMU_sub = n.subscribe("IMU", 1000, &mapper::update_location, &Map);
 
-  ros::Publisher map_pub = n.advertise<>("map", 1000);
+  ros::Publisher map_pub = n.advertise<sensor_msgs::PointCloud2>("map", 1000);
 
-  ros::Publisher location_pub = n.advertise<>("location", 1000);  
+  ros::Publisher location_pub = n.advertise<nav_msgs::Odometry>("location", 1000);  
 
   ros::Rate r(10);
 
@@ -131,12 +145,11 @@ int main(int argc, char **argv){
     sensor_msgs::PointCloud2 map_msg;
     nav_msgs::Odometry location_msg;
 
-    map_msg = Map.get_map_msg;
-    location_msg = Map.get_location_msg;  
+    map_msg = Map.get_map_msg();
+    location_msg = Map.get_location_msg();  
 
     map_pub.publish(map_msg);
     location_pub.publish(location_msg);
-    Map.publish_location();
 
     ros::spinOnce();
     r.sleep();
